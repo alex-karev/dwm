31ebbe421f82a34a2651ad3d4e8c3db3f28dbc0f
alex-karev <karev.as99@gmail.com>
2023-01-27 21:22:29 +0800

    Add Trayer rule

--- config.h
+++ config.h
@@ -71,6 +71,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                    instance    title       tags mask     isfloating   monitor */	
+	{ "trayer",                  NULL,       NULL,       0,            1,           -1 },
+
 	{ "Code",                    NULL,       NULL,       1 << 0,       0,           -1 },
 	{ "code-oss",                NULL,       NULL,       1 << 0,       0,           -1 },
 	  
